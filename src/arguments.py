17e9995510b04ef125d9a172ab8951b454da4c2d
jtankers <jim.tank.jr@gmail.com>
2019-05-18 14:47:47 -0500

    refactor for clarity and brevity

--- src/arguments.py
+++ src/arguments.py
@@ -9,46 +9,37 @@ def get_arg(n, default):
     return default
 
 
-def is_arg(n, value):
-  result = False
-  if len(sys.argv) > n:
-    if value == sys.argv[n]:
-      return True
-  return result
-
-
 class CommandHandler():
-  # opinionated class methods using non-opinionated functions
+  # defines argument[1] command consuming arguments[2:n]
 
   def __init__(self):
     self._commands = []
     self._command = ""
 
-  def arg(self, n, default_value):
-    return get_arg(n, default_value)
+  def arg(self, n, default):
+    return get_arg(n, default)
         
-  def equal(self, command):
-    # defines argument[1] as a command
+  def is_command(self, command):
     if not command in self._commands:
       self._commands.append(command)
-    if is_arg(1, command):
+    if get_arg(1, "") == command:
       self._command = command
       return True
     else:
       return False
-  
+
   def is_handled(self):
     if self._command in self._commands:
       return True
     else:
       return False
 
-  def get_help(self, title="", example="", options=[]):
+  def get_help(self, title="", example="", commands=[]):
     if len(title) > 0:
       print(title)
-    if len(options) == 0:
-      options = self._commands
+    if len(commands) == 0:
+      commands = self._commands
     if len(example) > 0:
       print(f"  example: {example}")
-    if len(options) > 0:
-      print(f"  options: {self._commands}")
+    if len(commands) > 0:
+      print(f"  commands: {self._commands}")
--- src/main.py
+++ src/main.py
@@ -8,17 +8,17 @@ from . import prime
 
 def execute():
   cmd = arguments.CommandHandler()
-  if cmd.equal("fib_for"):
-    fib.print_fib_for(int(cmd.arg(2, 10)))
-  if cmd.equal("fib_rec"):
+  if cmd.is_command("fib_for"):
+    fib.print_fib_for(int(cmd.arg(2, "10")))
+  if cmd.is_command("fib_rec"):
     fib.print_fib_rec(int(cmd.arg(2, 10)))
-  if cmd.equal("get_factors"):
+  if cmd.is_command("get_factors"):
     fact.print_get_factors(int(cmd.arg(2, 121)))
-  if cmd.equal("get_factorial"):
+  if cmd.is_command("get_factorial"):
     fact.print_get_factorial(int(cmd.arg(2, 10)))
-  if cmd.equal("is_prime"):
+  if cmd.is_command("is_prime"):
     prime.print_is_prime(int(cmd.arg(2, 11)))
-  if cmd.equal("is_factorial"):
+  if cmd.is_command("is_factorial"):
     fact.print_is_factorial(int(cmd.arg(2, 87178291200)))
   if not cmd.is_handled():
     cmd.get_help(
