083dcaa20f21e0f22ba95db6644c4eda20f2d5e7
jtankers <jim.tank.jr@gmail.com>
2019-05-19 11:38:41 -0500

    MIT license file

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+#!/bin/bash MIT (c) jtankersley 2019-05-18
 ./_venv/bin/python3 -m src $@
--- tests/test_timer.py
+++ tests/test_timer.py
@@ -1,4 +1,4 @@
-# MIT, jtankersley, 2019-05-18
+# MIT (c) jtankersley 2019-05-18
 import time
 import unittest
 from src import timer
