d2eff5816690a3d817ae9e2823d727be626722b4
jtankers <jim.tank.jr@gmail.com>
2019-05-19 14:53:04 -0500

    refine testing folder/file options

--- test.sh
+++ test.sh
@@ -4,12 +4,13 @@ if [ $# -gt 0 ]; then
     echo "Test file(s): $@"
     ./_venv/bin/python3 -m unittest $@
   else
-    echo "Test folder: $1"
-    ./_venv/bin/python3 -m unittest discover -s $1
+    echo "Test folder: ./$1"
+    ./_venv/bin/python3 -m unittest discover -s "./$1"
   fi
 else
-  echo "Help: Test <folder> or <file>"
+  echo "Help: Test <folder> or <file(s)>"
   echo "Examples:"
   echo "  ./test.sh tests"
-  echo "  ./test.sh tests/test_timer.py"
+  echo "  ./test.sh tests/test_sort.py"
+  echo "  ./test.sh */*_sort.py */*_timer.py"
 fi
