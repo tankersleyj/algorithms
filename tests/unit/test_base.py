5a5c1f57d76a614c8edfe25052d04317d9ebc13f
jtankers <jim.tank.jr@gmail.com>
2019-05-20 23:02:53 -0500

    convert bases

--- tests/unit/test_base.py
+++ tests/unit/test_base.py
@@ -11,3 +11,14 @@ class TestBase(unittest.TestCase):
         actual = timer.run_timed_result(base.convert_to_base_10, "11011", 2)
         expected = 27
         self.assertEqual(actual, expected, "convert_to_base_10")
+        actual = timer.run_timed_result(base.convert_to_base_10, "FF", 16)
+        expected = 255
+        self.assertEqual(actual, expected, "convert_to_base_10")
+
+    def test_convert_from_base_10(self):
+        # actual = timer.run_timed_result(base.convert_from_base_10, 27, 2)
+        # expected = "11011"
+        # self.assertEqual(actual, expected, "convert_from_base_10")
+        actual = timer.run_timed_result(base.convert_from_base_10, 255, 16)
+        expected = "FF"
+        self.assertEqual(actual, expected, "convert_to_base_10")
