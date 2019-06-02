14e9f73d959222529b25bffba960e17057e13660
jtankers <jim.tank.jr@gmail.com>
2019-06-02 18:10:07 -0500

    ddl tail

--- src/link.py
+++ src/link.py
@@ -18,11 +18,18 @@ class DoubleList():
 
     def __str__(self):
         string = ""
-        node = self.head
-        while node:
-            string += f"{str(node.data)},"
-            node = node.right
-        return string[:len(string) -1] if len(string) > 0 else ""
+        if self.head:
+            node = self.head
+            while node:
+                string += f"{str(node.data)},"
+                node = node.right
+            return string[:len(string) -1] if len(string) > 0 else ""
+        if self.tail:
+            node = self.tail
+            while node:
+                string = f"{str(node.data)},{string}"
+                node = node.left
+            return string[:len(string) -1] if len(string) > 0 else ""
 
     def addHead(self, data):
         if self.head:
@@ -41,6 +48,25 @@ class DoubleList():
             if self.head.right:
                 self.head = self.head.right
                 self.head.left = None
+            return node.data
+
+    def addTail(self, data):
+        if self.tail:
+            self.tail.right = DoubleNode(data, self.tail, None)
+            self.tail = self.tail.right
+        else:
+            self.tail = DoubleNode(data)
+
+    def peekTail(self):
+        if self.tail:
+            return self.tail.data
+
+    def popTail(self):
+        if self.tail:
+            node = self.tail
+            if self.tail.left:
+                self.tail = self.tail.left
+                self.tail.right = None
             return node.data
 
     # def addTail(self, data):
--- tests/unit/test_link.py
+++ tests/unit/test_link.py
@@ -6,28 +6,49 @@ from src import timer
 
 class TestSort(unittest.TestCase):
 
-    def test_link_dll(self):
+    def test_link_dllHead(self):
         dll = link.DoubleList()
         dll.addHead(1)
         dll.addHead(2)
         dll.addHead(3)
         actual = str(dll)
         expected = "3,2,1"
-        self.assertEqual(actual, expected, "test_link_dll.1")
+        self.assertEqual(actual, expected, "test_link_dllHead.1")
 
         actual = dll.peekHead()
         expected = 3
-        self.assertEqual(actual, expected, "test_link_dll.2")
+        self.assertEqual(actual, expected, "test_link_dllHead.2")
 
         actual = dll.popHead()
         expected = 3
-        self.assertEqual(actual, expected, "test_link_dll.3")
+        self.assertEqual(actual, expected, "test_link_dllHead.3")
 
         actual = str(dll)
         expected = "2,1"
-        self.assertEqual(actual, expected, "test_link_dll.4")
+        self.assertEqual(actual, expected, "test_link_dllHead.4")
 
-        # print(f"dll.head.data={dll.head.data}")
+    def test_link_dllTail(self):
+        dll = link.DoubleList()
+        dll.addTail(1)
+        dll.addTail(2)
+        dll.addTail(3)
+        actual = str(dll)
+        expected = "1,2,3"
+        self.assertEqual(actual, expected, "test_link_dllTail.1")
+
+        actual = dll.peekTail()
+        expected = 3
+        self.assertEqual(actual, expected, "test_link_dllTail.2")
+
+        actual = dll.popTail()
+        expected = 3
+        self.assertEqual(actual, expected, "test_link_dllTail.3")
+
+        actual = str(dll)
+        expected = "1,2"
+        self.assertEqual(actual, expected, "test_link_dllTail.4")
+
+        # print(f"dll.Tail.data={dll.head.data}")
         # print(f"dll={str(dll)}")
         # print(f"{str(dll)}")
         # print(f"{dll.toString()}")
