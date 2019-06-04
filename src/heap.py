b6e4fc7ff14cfca28b5ff3a59b4c4f786023ac84
jtankers <jim.tank.jr@gmail.com>
2019-06-03 21:18:36 -0500

    multi-value version of min/max heap

--- src/heap.py
+++ src/heap.py
@@ -20,7 +20,7 @@ class HeapType(Enum):
     MAXIMUM = 2
 
 
-""" Heap. """
+""" Basic Min or Max Heap, distinct keys, non-sorted. """
 class Heap():
 
     def __init__(self, type):
@@ -55,3 +55,53 @@ class Heap():
 
     def get_in_order_list(self):
         return self.heap
+
+
+""" Multi-value Heap. """
+class MultiHeap():
+
+    def __init__(self, type):
+        self.heap = []
+        self.count = []
+        self.type = type
+
+    def __str__(self):
+        """Print value in-order."""
+        return str(self.get_in_order_list())
+
+    def _swap(self, list, index1, index2):
+        save = list[index1]
+        list[index1] = list[index2]
+        list[index2] = save
+
+    def _balance_up(self, index):
+        if index > 0:
+            value = self.heap[index]
+            parent_index = (index - 1) // 2
+            parent_value = self.heap[parent_index]
+            if (self.type == HeapType.MAXIMUM and value > parent_value) or \
+                (self.type == HeapType.MINIMUM and value < parent_value):
+                self._swap(self.heap, index, parent_index)
+                self._swap(self.count, index, parent_index)
+                self._balance_up(parent_index)
+
+    def add(self, value):
+        last_index = len(self.heap) - 1
+        if last_index < 0:
+            self.heap.append(value)
+            self.count.append(1)
+        else:
+            last_value = self.heap[last_index]
+            if value == last_value:
+                self.count[last_index] += 1
+            else:
+                self.heap.append(value)
+                self.count.append(1)
+                self._balance_up(last_index)       
+
+    def get_in_order_list(self):
+        multiList = []
+        for index, value in enumerate(self.heap):
+            for i in range(self.count[index]):
+                multiList.append(value)
+        return multiList
