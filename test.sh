46ead91a778a5236ac50d6fbcc5da53f51a18c9f
jtankers <jim.tank.jr@gmail.com>
2019-06-15 13:50:13 -0500

    refactor to save primes to memory (todo, debug)

--- test.sh
+++ test.sh
@@ -3,7 +3,6 @@ if [ $# -eq 0 -o "$1" == '-h' -o "$1" == '--help' ]; then
   echo "Required: <folder> [<pattern>]"
   echo "Examples:"
   echo "  ./test.sh tests"
-  echo "  ./test.sh tests/unit"
   echo "  ./test.sh tests/unit *sort*"
 elif [ $# -eq 1 ]; then
   ./_venv/bin/python3 -m unittest discover -s "./$1"
