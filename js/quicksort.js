0095c5f709deb387a7cd72684ba44de2f3b6ceab
jtankers <jim.tank.jr@gmail.com>
2020-01-12 17:14:21 -0600

    js qs

--- js/quicksort.js
+++ js/quicksort.js
@@ -1,36 +1,45 @@
-// refactored from: https://www.guru99.com/quicksort-in-javascript.html
+// Quick Sort (log n)
+// refactored by J.Tankersley from: https://www.guru99.com/quicksort-in-javascript.html
 
-function swap(items, leftIndex, rightIndex){
+function qs_arraySwap(items, leftIndex, rightIndex){
     var temp = items[leftIndex];
     items[leftIndex] = items[rightIndex];
     items[rightIndex] = temp;
 }
 
-function partition(items, left, right) {
-    var pivot = items[Math.floor((right + left) / 2)], // middle element
+function qs_partition(items, left, right) {
+    var pivot = items[Math.floor((left + right) / 2)], // middle element
         i = left, // left pointer
         j = right; // right pointer
-    while (i <= j) {
+    while (i < j) {
         while (items[i] < pivot) {i++}
         while (items[j] > pivot) {j--}
-        if (i <= j) {swap(items, i++, j--)} // [de]increment i and j after call to swap
+        if (i < j) {qs_arraySwap(items, i++, j--)} // [de]increment i and j after call to swap
     }
     return i;
 }
 
-function quickSort(items, left, right) {
+function qs_recur(items, left, right) {
     var index;
     if (items.length > 1) {
-        index = partition(items, left, right); // index returned from partition
-        if (left < index - 1) {quickSort(items, left, index - 1)} // more elements on the left side of the pivot (+1)
-        if (right > index) {quickSort(items, index, right)} // more elements on the right side of the pivot
+        index = qs_partition(items, left, right); // index returned from partition
+        if (left < index - 1) {qs_partition(items, left, index - 1)} // more elements on the left side of the pivot (non-inclusive)
+        if (right > index) {qs_partition(items, index, right)} // more elements on the right side of the pivot (inclusive)
     }
     return items;
 }
 
-// first call to quick sort (highlightCode > rightClick > Run Code)
+function quickSort(items) {
+    return qs_recur(items, 0, items.length - 1);
+}
+
+// Test (Code Runner: cmd+a > right+click > Run Code | mac: ctrl+option+n)
 (function test_quickSort() {
-    var items = [5,3,7,6,2,9,1,-4,-9,0,8,-3];
-    var sortedArray = quickSort(items, 0, items.length - 1);
-    console.log(sortedArray); // prints [ -9, -4, -3, 0, 1, 2, 3, 5, 6, 7, 8, 9 ]
+    var items = [5,3,7,-2,6,2,9,1,-4,-9,0,8,-3];
+    console.log(quickSort(items)); // expect: [ -9, -4, -3, -2, 0, 1, 2, 3, 5, 6, 7, 8, 9 ]
+    console.log(items); // expect: [ -9, -4, -3, -2, 0, 1, 2, 3, 5, 6, 7, 8, 9 ]
+    console.log(quickSort([1])); // expect: [ 1 ]
+    console.log(quickSort([1,2])); // expect: [ 1, 2 ]
+    console.log(quickSort([5,3])); // expect: [ 3, 5 ]
+    console.log(quickSort([2,1,3])); // expect: [ 1, 2, 3 ]
 })()
