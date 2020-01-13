88bbc6b4e7a6994ead5ac7bd6326e4f0f86a1be8
jtankers <jim.tank.jr@gmail.com>
2020-01-13 15:24:42 -0600

    primes

---
+++ js/prime.js
@@ -0,0 +1,35 @@
+// MIT (c) jtankersley 2020-01-12, compare values and arrays
+// reference: // https://javascript.info/comparison
+
+function minFactor(value) {
+    let result=value, maxFactor=Math.floor(Math.sqrt(value)) + 1;
+    if (value>3) {
+        for (let i=2; i<=maxFactor; i++) {
+            if (value % i === 0) {
+                result=i;
+                break;
+            }
+        }
+    }
+    return result; // value or min factor > 1
+}
+
+function getPrimes(start, end) {
+    let primeArray = [];
+    for (let i=start; i<=end; i++) {
+        if (i!==2 && i===minFactor(i)) {primeArray.push(i)}
+    }
+    return primeArray;
+}
+
+// Test (Code Runner: ctrl+option+n)
+(function test() {
+    console.log(minFactor(1)) // expect 1
+    console.log(minFactor(2)) // expect 2
+    console.log(minFactor(4)) // expect 2
+    console.log(minFactor(7)) // expect 7
+    console.log(minFactor(49)) // expect 7
+    console.log(getPrimes(1,10)) // expect 1,3,5,7
+    console.log(getPrimes(11,50)) // expect 11,13,17,19,23,29,31,37,41,43,47
+    console.log(getPrimes(53,97)) // expect 53,59,61,67,71,73,79,83,89,97
+})()
